f773b48824bbd46c2d91fbd8227608a811c70f1c
ChanYu1224 <kinkitunekin@gmail.com>
2022-04-08 20:51:08 +0900

    進めた

---
+++ src/011.sh
@@ -0,0 +1,2 @@
+# "s/<before>/<after>/g"
+sed -e "s/\t/\s/g" ../data/popular-names.txt
---
+++ src/012.sh
@@ -0,0 +1,2 @@
+sed -n 1p ../data/popular-names.txt > ../data/col1.txt
+sed -n 2p ../data/popular-names.txt > ../data/col2.txt
