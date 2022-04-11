d6cfb6090392733f626008a94dbec99b29775952
ChanYu1224 <kinkitunekin@gmail.com>
2022-04-12 07:34:15 +0900

    進めた

--- src/027.py
+++ src/027.py
@@ -5,6 +5,6 @@ text = text_file.read()
 text_file.close()
 
 links = re.findall(r'\[\[.+?\]\]', text, re.MULTILINE | re.DOTALL)
-links_text = re.findall(r'\[\[([^\|#]+?).*?\]\]', text, re.MULTILINE | re.DOTALL)
+links_text = re.findall(r'\[\[(.+?)\]\]', text, re.MULTILINE | re.DOTALL)
 
 print(links_text)
